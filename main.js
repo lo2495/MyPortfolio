2e0631802403348406b9762a774ad34e2b763208
lo2495 <39268592+lo2495@users.noreply.github.com>
2024-01-22 20:28:13 +0800

    make changes

---
+++ main.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".text",{
+    strings:["Student","Junior Web Developer", "Junior Software Engineer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
+
+
